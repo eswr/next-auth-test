89936c5a8b6fb6ccb6271444cd9f3b1548f3b8df
Koka Easwarendra <koka.easwarendra@microsurgeonbot.com>
2024-03-06 00:22:45 +0530

    non-speech

--- components/demo/speech-to-text.tsx
+++ components/demo/speech-to-text.tsx
@@ -5,29 +5,29 @@ const SpeechToText = () => {
   const [transcript, setTranscript] = useState('');
   const [isListening, setIsListening] = useState(false);
 
-  useEffect(() => {
-    const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    const recognition = new speechRecognition();
+  // useEffect(() => {
+  //   const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  //   const recognition = new speechRecognition();
 
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    recognition.onresult = (event: { resultIndex: any; results: string | any[]; }) => {
-      console.log(event, 'event');
-      for (let i = event.resultIndex; i < event.results.length; ++i) {
-        if (event.results[i].isFinal) {
-          setTranscript(transcript + event.results[i][0].transcript);
-        }
-      }
-    };
+  //   recognition.continuous = true;
+  //   recognition.interimResults = true;
+  //   recognition.onresult = (event: { resultIndex: any; results: string | any[]; }) => {
+  //     console.log(event, 'event');
+  //     for (let i = event.resultIndex; i < event.results.length; ++i) {
+  //       if (event.results[i].isFinal) {
+  //         setTranscript(transcript + event.results[i][0].transcript);
+  //       }
+  //     }
+  //   };
 
-    if (isListening) {
-      recognition.start();
-    } else {
-      recognition.stop();
-    }
+  //   if (isListening) {
+  //     recognition.start();
+  //   } else {
+  //     recognition.stop();
+  //   }
 
-    return () => recognition.stop();
-  }, [isListening]);
+  //   return () => recognition.stop();
+  // }, [isListening]);
 
   return (
     <div>
